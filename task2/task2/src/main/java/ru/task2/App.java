dbce9a62fb0b2d4f40815841c0b5f4f04b3a0352
Сергей Олегович <s_perminov@bz.ru>
2023-10-19 16:02:37 +0300

    task 2.1 ready

--- task2/task2/src/main/java/ru/task2/App.java
+++ task2/task2/src/main/java/ru/task2/App.java
@@ -1,10 +1,27 @@
 package ru.task2;
 
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.IntStream;
+
 public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // 1. Написать метод, возвращающий количество чётных элементов массива.
+        // Пример: countEvens([2, 1, 2, 3, 4]) → 3 countEvens([2, 2, 0]) → 3 countEvens([1, 3, 5]) → 0
+        var numbers = IntStream.generate(() -> new Random().nextInt(100)).limit(20).toArray();
+        System.out.println("countEvens(" + Arrays.toString(numbers) + ") -> " + countEvens(numbers));
+
+
+        //2. Написать функцию, возвращающую разницу между самым большим и самым ма- леньким элементами переданного не пустого массива.
+
+
+        //3. Написать функцию, возвращающую истину, если в переданном массиве есть два соседних элемента, с нулевым значением.
+    }
+
+    private static int countEvens(int[] numbers){
+        return (int) Arrays.stream(numbers).filter(num -> num % 2 == 0).count();
     }
 }
