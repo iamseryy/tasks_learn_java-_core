047347db32575aa105d17f3afdb502df142bc0df
Сергей Олегович <s_perminov@bz.ru>
2023-10-20 09:26:15 +0300

    task2 ready

--- task2/task2/src/main/java/ru/task2/App.java
+++ task2/task2/src/main/java/ru/task2/App.java
@@ -1,9 +1,6 @@
 package ru.task2;
 
-
 import java.util.Arrays;
-import java.util.Random;
-import java.util.stream.IntStream;
 
 public class App
 {
@@ -11,17 +8,29 @@ public class App
     {
         // 1. Написать метод, возвращающий количество чётных элементов массива.
         // Пример: countEvens([2, 1, 2, 3, 4]) → 3 countEvens([2, 2, 0]) → 3 countEvens([1, 3, 5]) → 0
-        var numbers = IntStream.generate(() -> new Random().nextInt(100)).limit(20).toArray();
-        System.out.println("countEvens(" + Arrays.toString(numbers) + ") -> " + countEvens(numbers));
+        //var numbers = IntStream.generate(() -> new Random().nextInt(100)).limit(20).toArray();
+        var numbers = new int[]{1,3,44,5,1,0,44,55};
+        System.out.println("1. countEvens(" + Arrays.toString(numbers) + ") -> " + countEvens(numbers));
 
-
-        //2. Написать функцию, возвращающую разницу между самым большим и самым ма- леньким элементами переданного не пустого массива.
-
+        //2. Написать функцию, возвращающую разницу между самым большим и самым маленьким элементами переданного не пустого массива.
+        System.out.println((numbers.length > 0 ? "\n2. Max and min array item difference: " + minMaxArrayDifference(numbers) : "\n2. Empty array"));
 
         //3. Написать функцию, возвращающую истину, если в переданном массиве есть два соседних элемента, с нулевым значением.
+        System.out.println(numbers.length > 0 ? "\n3. Exists neighboring items in zero: " + existsNeighboringItemsInZero(numbers) : "\n3. Empty array");
     }
 
     private static int countEvens(int[] numbers){
         return (int) Arrays.stream(numbers).filter(num -> num % 2 == 0).count();
+    }
+
+    private static int minMaxArrayDifference(int[] numbers){
+        return Arrays.stream(numbers).max().orElse(0) - Arrays.stream(numbers).min().orElse(0);
+    }
+
+    private static boolean existsNeighboringItemsInZero(int[] numbers){
+        for(int i = 1; i < numbers.length; i++){
+            if (numbers[i - 1] - numbers[i] == 0) return true;
+        }
+        return false;
     }
 }
