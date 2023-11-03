da53c96b0d69dc4c92fd0929d1fcd1a9792aef1a
Сергей Олегович <s_perminov@bz.ru>
2023-11-03 12:39:49 +0300

    task5 ready

--- task5/Students/src/main/java/org/task5/App.java
+++ task5/Students/src/main/java/org/task5/App.java
@@ -1,5 +1,7 @@
 package org.task5;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -8,29 +10,40 @@ public class App
 {
     public static void main( String[] args ){
         var students = new ArrayList<>(List.of(
-                new Student("Student1", new ArrayList<>(List.of(4,5,3,4,4,5)), Specialty.CHEMISTRY),
-                new Student("Student2", new ArrayList<>(List.of(5,5,5,5,4,4)), Specialty.COMPUTER_SCIENCE),
-                new Student("Student3", new ArrayList<>(List.of(4,5,3,4,4,4)), Specialty.PHYSICS),
-                new Student("Student4", new ArrayList<>(List.of(4,5,3,4,4,3)), Specialty.COMPUTER_SCIENCE),
-                new Student("Student5", new ArrayList<>(List.of(4,5,3,3,4,2)), Specialty.CHEMISTRY),
-                new Student("Student6", new ArrayList<>(List.of(4,5,3,4,4,1)), Specialty.COMPUTER_SCIENCE),
-                new Student("Student7", new ArrayList<>(List.of(4,5,3,3,4,5)), Specialty.CHEMISTRY),
-                new Student("Student8", new ArrayList<>(List.of(4,5,5,5,5,5)), Specialty.COMPUTER_SCIENCE),
-                new Student("Student9", new ArrayList<>(List.of(3,3,3,3,4,5)), Specialty.PHYSICS),
-                new Student("Student10", new ArrayList<>(List.of(5,3,5,4,5,3)), Specialty.COMPUTER_SCIENCE),
-                new Student("Student11", new ArrayList<>(List.of(4,2,3,3,2,5)), Specialty.CHEMISTRY),
-                new Student("Student12", new ArrayList<>(List.of(3,3,3,5,4,3)), Specialty.COMPUTER_SCIENCE),
-                new Student("Student13", new ArrayList<>(List.of(4,4,3,3,4,5)), Specialty.PHYSICS),
-                new Student("Student14", new ArrayList<>(List.of(3,3,3,3,3,3)), Specialty.COMPUTER_SCIENCE)
+                new Student("Student1", new ArrayList<>(List.of(4.0,5.0,3.0,4.0,4.0,5.0)), Specialty.CHEMISTRY),
+                new Student("Student2", new ArrayList<>(List.of(2.0,3.0,4.0,5.0,5.0,5.0)), Specialty.COMPUTER_SCIENCE),
+                new Student("Student3", new ArrayList<>(List.of(4.0,5.0,3.0,4.0,4.0,4.0)), Specialty.PHYSICS),
+                new Student("Student4", new ArrayList<>(List.of(5.0, 5.0, 5.0)), Specialty.COMPUTER_SCIENCE),
+                new Student("Student5", new ArrayList<>(List.of(4.0,5.0,3.0,3.0,4.0,2.0)), Specialty.CHEMISTRY),
+                new Student("Student6", new ArrayList<>(List.of(4.0,5.0,5.0,5.0,4.0,5.0)), Specialty.COMPUTER_SCIENCE),
+                new Student("Student7", new ArrayList<>(List.of(4.0,5.0,3.0,3.0,4.0,5.0)), Specialty.CHEMISTRY),
+                new Student("Student8", new ArrayList<>(List.of(5.0,5.0,5.0,5.0,4.0,4.0)), Specialty.COMPUTER_SCIENCE),
+                new Student("Student9", new ArrayList<>(List.of(3.0,3.0,3.0,3.0,4.0,5.0)), Specialty.PHYSICS),
+                new Student("Student10", new ArrayList<>(List.of(5.0,3.0,5.0,4.0,5.0,3.0)), Specialty.COMPUTER_SCIENCE),
+                new Student("Student11", new ArrayList<>(List.of(4.0,2.0,3.0,3.0,2.0,5.0)), Specialty.CHEMISTRY),
+                new Student("Student12", new ArrayList<>(List.of(5.0,3.0,3.0,5.0,5.0,5.0, 5.0, 5.0, 5.0)), Specialty.COMPUTER_SCIENCE),
+                new Student("Student13", new ArrayList<>(List.of(4.0,4.0,3.0,3.0,4.0,5.0)), Specialty.PHYSICS),
+                new Student("Student14", new ArrayList<>(List.of(5.0,5.0,5.0,5.0,5.0,4.0)), Specialty.COMPUTER_SCIENCE),
+                new Student("Student15", new ArrayList<>(List.of(5.0,3.0,5.0,4.0,5.0,3.0)), Specialty.PHYSICS),
+                new Student("Student16", new ArrayList<>(List.of(5.0,3.0,5.0,5.0,5.0,5.0)), Specialty.COMPUTER_SCIENCE),
+                new Student("Student17", new ArrayList<>(List.of(5.0,3.0,5.0,4.0,5.0,3.0)), Specialty.CHEMISTRY)
+
         ));
 
         students.stream()
                 .filter(student -> student.specialty().equals(Specialty.COMPUTER_SCIENCE))
-                .filter(student -> student.assessments().stream()
-                        .mapToInt(assessment -> assessment)
-                        .average()
-                        .getAsDouble() > 4.5)
+                .filter(student -> student.grades().stream()
+                                                    .mapToDouble(grade -> grade)
+                                                    .average()
+                                                    .orElse(0.0) > 4.5)
+                .sorted(Comparator.comparingDouble(student-> student.grades().stream()
+                                                                                .mapToDouble(grade -> grade)
+                                                                                .average()
+                                                                                .orElse(0.0) * (-1)))
                 .limit(5)
-                .forEach(System.out::println);
+                .forEach(student -> System.out.println(student + "; average grade = " + student.grades().stream()
+                                                                                            .mapToDouble(grade -> grade)
+                                                                                            .average()
+                                                                                            .orElse(0.0)));
     }
 }
--- task5/Students/src/main/java/org/task5/Student.java
+++ task5/Students/src/main/java/org/task5/Student.java
@@ -2,5 +2,5 @@ package org.task5;
 
 import java.util.List;
 
-public record Student(String name, List<Integer> assessments, Specialty specialty) {
+public record Student(String name, List<Double> grades, Specialty specialty) {
 }
